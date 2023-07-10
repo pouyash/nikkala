4824beddaaa5a45d19447eaf2d90c4a26a8adac8
pouyash <qartalsh@gmail.com>
2023-07-10 22:30:29 +0330

    product detail

--- product/urls.py
+++ product/urls.py
@@ -4,4 +4,5 @@ from product import views
 
 urlpatterns = [
     path('<search>/', views.ProductsView.as_view(), name='products'),
+    path('product/<slug>', views.ProductDetailView.as_view(), name='product'),
 ]
--- product/views.py
+++ product/views.py
@@ -2,7 +2,7 @@ from django.db.models import Q
 from django.shortcuts import render
 
 # Create your views here.
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 from product.models import Product
 
@@ -12,8 +12,18 @@ class ProductsView(ListView):
     model = Product
     paginate_by = 1
     context_object_name = 'products'
-    # def get_queryset(self):
-    #     qs = super(ProductsView, self).get_queryset()
-    #     search = self.kwargs.get('search')
-    #     qs = qs.filter(Q(brand__title__contains=search) | Q(category__title__contains=search))
-    #     return qs
+    def get_queryset(self):
+        qs = super(ProductsView, self).get_queryset()
+        search = self.kwargs.get('search')
+        qs = qs.filter(Q(brand__title__contains=search) | Q(category__title__contains=search))
+        if len(qs) > 0:
+            return qs
+        else:
+            return super(ProductsView, self).get_queryset()
+
+
+class ProductDetailView(DetailView):
+    template_name = 'product/product.html'
+    context_object_name = 'product'
+    model = Product
+    slug_field = 'slug'
